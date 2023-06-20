bce08ca59c8b057ede03a62a8b8762d23fac323c
MatejBendik <matejbendik.mb@gmail.com>
2023-06-20 23:59:00 +0200

    Add titles to icon images

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,6 +18,7 @@ const Skills = () => {
               <img
                 src={skills[0].image}
                 alt="TypeScript Icon"
+                title={skills[0].name}
                 className="w-14 h-14"
               />
             </div>
@@ -25,6 +26,7 @@ const Skills = () => {
               <img
                 src={skills[1].image}
                 alt="React Icon"
+                title={skills[1].name}
                 className="w-14 h-14"
               />
             </div>
@@ -32,6 +34,7 @@ const Skills = () => {
               <img
                 src={skills[2].image}
                 alt="Tailwind Icon"
+                title={skills[2].name}
                 className="w-14 h-14"
               />
             </div>
@@ -41,12 +44,18 @@ const Skills = () => {
           <h2 className="mb-2">Back-End</h2>
           <div className="flex justify-start space-x-6">
             <div>
-              <img src={skills[3].image} alt="Node Icon" className="h-14" />
+              <img
+                src={skills[3].image}
+                alt="Node Icon"
+                title={skills[3].name}
+                className="h-14"
+              />
             </div>
             <div>
               <img
                 src={skills[4].image}
                 alt="Express Icon"
+                title={skills[4].name}
                 className="w-14 h-14"
               />
             </div>
@@ -54,18 +63,25 @@ const Skills = () => {
               <img
                 src={skills[5].image}
                 alt="MongoDB Icon"
+                title={skills[5].name}
                 className="w-14 h-14"
               />
             </div>
           </div>
           <div className="flex justify-start space-x-6 mt-2">
             <div>
-              <img src={skills[6].image} alt="PHP Icon" className="w-14 h-14" />
+              <img
+                src={skills[6].image}
+                alt="PHP Icon"
+                title={skills[6].name}
+                className="w-14 h-14"
+              />
             </div>
             <div>
               <img
                 src={skills[7].image}
                 alt="MySQL Icon"
+                title={skills[7].name}
                 className="w-14 h-14"
               />
             </div>
@@ -75,18 +91,25 @@ const Skills = () => {
           <h2 className="mb-2">Other</h2>
           <div className="flex justify-start space-x-6">
             <div>
-              <img src={skills[8].image} alt="Git Icon" className="w-14 h-14" />
+              <img
+                src={skills[8].image}
+                alt="Git Icon"
+                title={skills[8].name}
+                className="w-14 h-14"
+              />
             </div>
             <div>
               <img
                 src={skills[9].image}
                 alt="GitHub Icon"
+                title={skills[9].name}
                 className="w-14 h-14"
               />
             </div>
             <div>
               <img
                 src={skills[10].image}
+                title={skills[10].name}
                 alt="Linux Icon"
                 className="w-14 h-14"
               />
@@ -94,11 +117,17 @@ const Skills = () => {
           </div>
           <div className="flex justify-start space-x-9 mt-2 ml-2">
             <div>
-              <img src={skills[11].image} alt="Figma Icon" className="h-14" />
+              <img
+                src={skills[11].image}
+                alt="Figma Icon"
+                title={skills[11].name}
+                className="h-14"
+              />
             </div>
             <div>
               <img
                 src={skills[12].image}
+                title={skills[12].name}
                 alt="Neovim Icon"
                 className="w-14 h-14"
               />
